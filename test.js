0994996bdd0b489a20d6aa50c64fb9a53b862bd7
Yoshua Wuyts <i@yoshuawuyts.com>
2015-09-05 01:00:13 +1000

    hash: fix trailing / bug

--- test.js
+++ test.js
@@ -10,6 +10,11 @@ test('#test and #/test should return /test', function (t) {
   t.end();
 });
 
+test('#test/ should return /test', function (t) {
+  t.plan(1)
+  t.equal(match('#test/'), '/test')
+})
+
 test('#/prefix/test and #prefix/test should return /test', function (t) {
   var a = match('#test', '/prefix');
   t.equal(a, '/test');
